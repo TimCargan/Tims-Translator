e4b3ba71bd27d46e51d4372730ced06288cc5d4d
Tim Cargan <timcargan@gmail.com>
2014-09-29 18:26:57 -0400

    Initial

---
+++ tools/formatData.py
@@ -0,0 +1,41 @@
+text = open("testData.txt").read()
+#noBreak = open(noB)
+output = open ("split.en", "w")
+
+def isEndOfLine(text):
+	return true
+
+def splitParagaphs():
+	p = text.split("\n")
+	return p
+
+def removeNoLang(p):
+	for line in p:
+		if line != "":
+			if line[0] == '<':
+				p.remove(line)
+	return p
+
+def splitSentances(text):
+	allSentacnes = []
+	for p in text:
+		sentances = p.split(". ")
+		#need to add exeptions
+		#sentances = rejoin(sentances)
+		allSentacnes += sentances
+	return allSentacnes
+
+def write(p):
+	for line in p:
+		if line.strip() == "":
+			continue
+		output.write(line + "\n")
+
+print "Running..."
+p = splitParagaphs()
+print "Praragaphs split..."
+p = removeNoLang(p)
+p = splitSentances(p)
+write(p)
+output.close()
+print "done"
---
+++ tools/preProcessor.py
@@ -0,0 +1,89 @@
+import pickle
+
+print "opening file"
+rawText = open("english.en").read()
+rawText = rawText.split("\n")
+
+trigrams = {}
+bigrams = {}
+unigrams = {}
+
+#Bigest flaw is that it trigram cant deal with less than 3 words
+def preProcess(line, c, l):
+	pString = line.split(" ")
+	for word in range(-2, len(pString)):
+		x = getFromAray(pString, word)
+		y = getFromAray(pString, word + 1)
+		z = getFromAray(pString, word + 2)
+
+		trigram = join([x, y, z])
+		bigram1 = join([x,y])
+		bigram2 = join([y,z])
+		unigram = z
+
+		trigam = hash(trigram)
+		bigram1 = hash(bigram1)
+		bigram2 = hash(bigram2)
+		unigram = hash(unigram)
+
+		if trigram in  trigrams:
+			trigrams[trigram] += 1
+		else:
+			trigrams[trigram] = 1
+
+		if bigram1 in  bigrams:
+			bigrams[bigram1] += 1
+		else:
+			bigrams[bigram1] = 1
+
+		if bigram2 in  bigrams:
+			bigrams[bigram2] += 1
+		else:
+			bigrams[bigram2] = 1
+
+		if unigram in  unigrams:
+			unigrams[unigram] += 1
+		else:
+			unigrams[unigram] = 1
+		
+
+	print "Processed {:d}/{:d}: {:s}".format(line, c, l)
+
+
+
+#get element for array or reutrn "" if index is not valid
+def getFromAray(aray, index):
+	if index < 0 :
+		return "*#es#*"
+	try:
+		return aray[index]
+	except IndexError: 
+		return "*#es#*"
+
+#join an array of words into a string
+def join(strings):
+	r = ""
+	for word in strings:
+		r += word +  " "
+	return r.strip()
+
+def save(obj, name ):
+    with open('obj/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load(name ):
+    with open('obj/' + name + '.pkl', 'r') as f:
+        return pickle.load(f)
+
+count = 1
+lenght = len(rawText)
+print "File open, starting processing..."
+for line in rawText:
+	preProcess(line, count, length)
+
+print "Saving..."
+save(trigrams, "trigrams")
+print "Saving..."
+save(bigrams, "bigrams")
+print "Saving..."
+save(unigrams, "unigrams")
